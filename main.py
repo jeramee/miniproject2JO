2840df3402c8a99b3e5c773ece827ccc142b5f38
jeramee <jerameeoliver@gmail.com>
2023-09-24 23:05:37 -0500

    Almost done now. Creating Graphs saving to file.

--- main.py
+++ main.py
@@ -5,9 +5,11 @@
 # (5/5 points) Initial comments with your name, class and project at the top of your .py file.
 
 # (5/5 points) Proper import of packages used.
-import pandas as pd
-from datetime import datetime
 import re
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
 
 '''
 data = pd.read_csv('artists.csv', index_col='Artist')
@@ -15,28 +17,30 @@ data = pd.read_csv('artists.csv', index_col='Artist')
 print(data.head())
 '''
 
+# Start of program
+# Create our charts folder
+try:
+    Path("charts").mkdir()
+except FileExistsError:
+    pass
+
 df = pd.read_csv('US-Presidents.csv')
 
 column_names = df.columns.tolist()
-print(column_names)
+#print(column_names)
 
 
 net_worth = df['Net worth((millions of 2022 US$))']
 
-print(net_worth)
+#print(net_worth)
 
 comparison_table = df[['Age', 'Net worth((millions of 2022 US$))', 'Political party[11]', 'Years in office', 'IQ']]
-print(comparison_table)
+#print(comparison_table)
 
-print(df['Education'])
+#print(df['Education'])
 
 term = df['Years in office']
-print(term)  # if you want to print and see the contents of term
-
-
-import re
-from datetime import datetime
-import pandas as pd
+#print(term)  # if you want to print and see the contents of term
 
 
 def calculate_years(term):
@@ -58,26 +62,68 @@ def calculate_years(term):
 df['Total Years'] = df['Years in office'].apply(calculate_years)
 
 # print out the results
-print(df['Total Years'])
+#print(df['Total Years'])
 
 
 df['IQ'] = df['IQ'].fillna('120')
 df['IQ'] = df['IQ'].apply(lambda x: max(map(int, re.findall(r'\d+', x))))
-print(df['IQ'])
+#print(df['IQ'])
 
 column_names = df.columns.tolist()
-print(column_names)
+#print(column_names)
 
-df['Net worth((millions of 2022 US$))'] = df['Net worth((millions of 2022 US$))'].replace('<1', '1')
+# The regex \D+ means "one or more non-digit characters".
+df['Net worth((millions of 2022 US$))'] = df['Net worth((millions of 2022 US$))'].str.replace('\D+', '', regex=True)
+
+# Convert to integer
+df['Net worth((millions of 2022 US$))'] = df['Net worth((millions of 2022 US$))'].astype(int)
+
 print(df['Net worth((millions of 2022 US$))'])
 
+
 df['Education'] = df['Education'].apply(lambda x: 0 if 'No formal education' in x else 1 if 'did not graduate' in x else 2)
-print(df['Education'])
+#print(df['Education'])
+
+# In this line, we first split the string on spaces, take the first part,
+# then use a regex to replace all non-numeric characters with an empty string,
+# and finally, convert the result to integers.
+df['Age'] = df['Age'].str.split(' ').str[0].replace('\D+', '', regex=True).astype(int)
+
+#print(df['Age'])
 
 
-print(df['Age'])
+
+# Code for Comparing Networth to Age
+plt.figure(figsize=(10,6))
+plt.scatter(df['Age'], df['Net worth((millions of 2022 US$))'])
+plt.xlabel('Age')
+plt.ylabel('Net worth((millions of 2022 US$))')
+plt.title('Comparing Net worth with Age')
+plt.show()
+plt.title('Comparing Net worth with Age')
+plt.savefig('charts/age_vs_net_worth.png')  # save the plot as a png file in "charts" directory
 
 
+
+# Code for Comparing Networth to Political Party
+color_dict = {'Democratic': 'blue', 'Republican': 'red'}
+
+# Sort dataframe by 'Net worth((millions of 2022 US$))'
+df = df.sort_values('Net worth((millions of 2022 US$))', ascending=True)
+
+plt.figure(figsize=(10,6))
+
+for party, color in color_dict.items():
+    plt.scatter(df[df['Political party[11]'] == party]['Political party[11]'],
+                df[df['Political party[11]'] == party]['Net worth((millions of 2022 US$))'],
+                color=color)
+
+plt.xlabel('Political party[11]')
+plt.ylabel('Net worth((millions of 2022 US$))')
+plt.title('Comparing Net worth with Political Party')
+plt.show()
+plt.title('Comparing Net worth with Political Party')
+plt.savefig('charts/networth_vs_political_party.png') # save the plot as a png file in "charts" directory
 
 '''
 import yfinance as yf
